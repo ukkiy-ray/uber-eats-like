a981029026757f00a42e929f7b81f06d5dcbbe76
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-08 16:54:46 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+
+        <Route
+          exact
+          path="/restaurants">
+          <Restaurants />
+        </Route>
+
+        <Route
+          exact
+          path="/restaurants/:restaurantsId/foods"
+          render={({ match }) =>
+            <Foods
+              match={match}
+            />
+          }
+        />
+
+        <Route
+          exact
+          path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
